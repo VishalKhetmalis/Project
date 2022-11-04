4b5611626922498e1c06043605ab7f8479c29499
Vishal Rajendra Khetmalis <vishalkhetmalis.dbdamarch22@gmail.com>
2022-11-04 14:05:42 +0530

    Changes 2

--- Swap Variables.py
+++ Swap Variables.py
@@ -1,11 +1,9 @@
 # Creating variables
-HEAD
 num1 = 10
 num2 = 25
 
 num1 = 40
 num2 = 60
-V1.0
 
 # Printing variables
 print("Variables before swapping are num1 : {} & num2 : {}".format(num1,num2))
